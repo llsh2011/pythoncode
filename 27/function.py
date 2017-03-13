680fdcd9d62e79ce3d3b0b607d88d2ec3df733c5
llsh2011 <llsh10129@qq.com>
2017-03-13 21:46:59 +0800

    add function

---
+++ 27/function.py
@@ -0,0 +1,21 @@
+#encoding=utf-8
+
+# -*- coding: utf-8 -*-
+
+
+print max(1,2)
+print'max(1,2) = ',max(1,2)
+
+print 'int(\'123\')=',int('123')
+
+
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+		
+
+print my_abs(2)
+print my_abs(-2)
